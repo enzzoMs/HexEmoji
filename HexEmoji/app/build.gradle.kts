da75dc9cc7fbf43c7b503ca0ba4a774a7e1f4728
Enzzo Morais de Olinda <enzzomorais0@gmail.com>
2024-05-27 15:19:52 -0300

    Fixing missing translations

--- HexEmoji/app/build.gradle.kts
+++ HexEmoji/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sarueh.hexemoji"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "1.1"
+        versionCode = 23
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
